79e8d28e2e7d93197e741c472a975f923bbe992b
Francisco Javier Monleon Galiana <dumbo.cat@hotmail.com>
2020-08-03 09:35:00 +0100

    añadir headers y footers a paginas

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- codigo para poder tener caracteres en español y acentos -->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<html lang="es">                                                                  <!-- bootstrap -->
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<title>VARADERO</title>
+   </head>
+<body>
+  
+<!--poner pagina color negro en css y letra blanca-->
+   <style>
+      body {background-color: black;}
+      color :{white};
+   </style>
